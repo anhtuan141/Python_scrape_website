f14c5ab4daae0c1f6e892d0cbabd506eaea2b497
anhtuan141 <anhtuan1411994@gmail.com>
2023-11-10 23:33:57 +0700

    write logic Scrape chapter list

--- example.py
+++ example.py
@@ -36,15 +36,31 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 # Begin Scrape
 results = soup.find(id="truyen")
+resultsChapter = soup.find(id="chapter-list")
 
 # Scrape story name
 story_name = results.find("h3", class_="title")
-print(story_name.text)
+print(f"Story name: {story_name.text}")
 print()
 
 # Scrape story description
 story_description = results.find("div", class_="story-desc")
 inner_html = story_description.contents
 
+# Extract description
 for element in inner_html:
-    print(element)
+    print(f"Story description: {element}")
+
+print()
+
+# Scrape chapter list
+chapter_list = resultsChapter.find("ul", class_="list-chapter")
+
+# Find all the <a> tags within the <ul> for chapters
+chapter_links = chapter_list.find_all("a")
+
+# Extract chapter information
+for link in chapter_links:
+    chapter_url = link.get("href")
+    chapter_title = link.get("title")
+    print(f"Chapter Title: {chapter_title}, Chapter URL: {chapter_url}")
